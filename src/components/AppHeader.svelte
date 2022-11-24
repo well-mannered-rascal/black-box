1177e49848c62151a38172587bdebba0ef6d39fc
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-23 21:06:07 -0700

    init

---
+++ src/components/AppHeader.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import {SettingsIcon} from 'svelte-feather-icons'
+
+    export let projectTitle: String = '';
+
+</script>
+
+<div class="app-header">
+    {#if projectTitle}
+        <span class="app-icon">Bb</span> <!-- TODO Links to project page -->
+        <span class="app-title">{projectTitle}</span>
+    
+    {:else}
+        <span class="app-title">Blackbox</span>
+    {/if}
+
+    <SettingsIcon size="1.25x" class="feather-icon"/>
+</div>
+
+<style>
+    .app-header {
+        height: 50px;
+        background-color: rgb(20, 20, 20);
+        padding: 10px 15px;
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid rgb(146, 146, 146);
+        color: white;
+        font-size: larger;
+        justify-content: space-between;
+        margin-bottom: 10px;
+        font-weight: bold;
+        letter-spacing: 10px;
+    }
+    .app-title {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space:nowrap;
+        max-width: 55vw;
+    }
+</style>
