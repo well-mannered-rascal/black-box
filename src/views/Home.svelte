3a6b16bfdea40677cefb99d6277f196d6a26230b
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-23 21:07:14 -0700

    init, home page view

---
+++ src/views/Home.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { PlusIcon } from "svelte-feather-icons";
+  import AppHeader from "../components/AppHeader.svelte";
+  import ProjectListItem from "../components/ProjectListItem.svelte";
+
+  const createNewProject = () => {
+    console.log("click");
+  };
+</script>
+
+<div>
+  <AppHeader />
+  <div class="project-item-container">
+    <!-- TODO Load project information from state management here, sort alphabetical -->
+    {#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] as projectNum}
+      <ProjectListItem projectTitle={`Project ${projectNum}`} />
+    {/each}
+  </div>
+  <button
+    class="new-project-button"
+    on:click={createNewProject}
+  >
+    <PlusIcon size="2.5x" />
+  </button>
+</div>
+
+<style>
+  .project-item-container {
+    padding: 20px;
+    width: 100%;
+    height: 90vh;
+    padding-bottom: 75px;
+    overflow: auto;
+  }
+  .new-project-button {
+    all: unset;
+    cursor: pointer;
+    background-color: rgb(255, 255, 255);
+    width: 65px;
+    height: 65px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 30px;
+    right: 20px;
+  }
+  .new-project-button:active {
+    background-color: rgb(200, 200, 200);
+  }
+</style>
