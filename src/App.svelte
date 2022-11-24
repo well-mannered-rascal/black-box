6ae9e47071f3a50823b26530d2d097f96410f6d5
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-23 21:05:55 -0700

    Working towards page routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
-</script>
+  import {MenuIcon, PlusIcon} from 'svelte-feather-icons'
+  import AppHeader from './components/AppHeader.svelte';
+  import Home from './views/Home.svelte'
 
+
+  
+</script>
 <main>
+  
+  <Home />
+  
+  
 </main>
 
+
+
+<style>
+  
+  
+</style>
