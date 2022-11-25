2b2a61ff0a7058a0b338b11ddce5df058c5784a9
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-24 21:10:39 -0700

    init

---
+++ src/views/Project.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import AppHeader from "../components/AppHeader.svelte";
+
+  export let props;
+
+  const { projectId } = props;
+
+  console.log(projectId);
+</script>
+
+<div>
+  <AppHeader projectTitle={`project ${projectId}`} />
+</div>
+
+<style></style>
