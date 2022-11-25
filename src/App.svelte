f1cdb7f9bdc60cc6dbb282de362eb16829ddcf17
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-24 21:09:11 -0700

    Add dynamic routing and view loading via page

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-  import {MenuIcon, PlusIcon} from 'svelte-feather-icons'
-  import AppHeader from './components/AppHeader.svelte';
-  import Home from './views/Home.svelte'
+  import page from "page";
+  import Home from "./views/Home.svelte";
+  import Project from "./views/Project.svelte";
 
+  let currentView = Home;
+  let props = {};
 
-  
+  page("/", () => {
+    currentView = Home;
+  });
+  page("/project/:id", (context) => {
+    props["projectId"] = context.params.id;
+    currentView = Project;
+  });
+  page.start();
 </script>
+
 <main>
-  
-  <Home />
-  
-  
+  <svelte:component
+    this={currentView}
+    {props}
+  />
 </main>
 
-
-
 <style>
-  
-  
 </style>
