7fec7d875cddcb5d78be0c551c013eb367e2ea35
Tom Amaral <thomasamaral2016@gmail.com>
2022-11-26 14:50:45 -0700

    Extract model to types module

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,30 +1,7 @@
 import Dexie from "dexie";
 import type { Table } from "dexie";
 
-export interface Step {
-  active: boolean;
-  accent?: boolean;
-}
-
-export interface PatternState {
-  [note: string]: Step[];
-}
-
-export interface Pattern {
-  readonly id: number;
-  bpm?: number;
-  scale: string; // i.e. 'chromatic, 'c major', 'c# minor', etc
-  stepCount: number;
-  /**(optional) How many steps per beat */
-  subdivision?: number;
-  state: PatternState;
-}
-
-export interface Project {
-  readonly id: number;
-  title: string;
-  patterns: Pattern[];
-}
+import type { Project } from "./types";
 
 export class DexieDB extends Dexie {
   project!: Table<Project>;
---
+++ src/lib/types.ts
@@ -0,0 +1,25 @@
+export interface Step {
+  active: boolean;
+  accent?: boolean;
+  index: number;
+}
+
+export interface PatternState {
+  [note: string]: Step[];
+}
+
+export interface Pattern {
+  readonly id: number;
+  bpm?: number;
+  scale: string; // i.e. 'chromatic, 'c major', 'c# minor', etc
+  stepCount: number;
+  /**(optional) How many steps per beat */
+  subdivision?: number;
+  state: PatternState;
+}
+
+export interface Project {
+  readonly id: number;
+  title: string;
+  patterns: Pattern[];
+}
