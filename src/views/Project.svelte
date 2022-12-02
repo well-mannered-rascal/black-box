1cd0ba0f91a9deb3b6b064c4e998ed179352b94b
Tom Amaral <thomasamaral2016@gmail.com>
2022-12-01 21:38:29 -0700

    Dynamically load project information from the DB

--- src/views/Project.svelte
+++ src/views/Project.svelte
@@ -1,15 +1,79 @@
 <script lang="ts">
+  import type { Project } from "../lib/types";
+  import { blackboxDB } from "../lib/db";
   import AppHeader from "../components/AppHeader.svelte";
+  import { liveQuery } from "dexie";
 
   export let props;
+  const projectId = parseInt(props.projectId);
 
-  const { projectId } = props;
-
-  console.log(projectId);
+  let projectQuery = blackboxDB.project.get(projectId);
+  let selectedPattern = 0;
 </script>
 
 <div>
-  <AppHeader projectTitle={`project ${projectId}`} />
+  {#await projectQuery then project}
+    <AppHeader projectTitle={project.title} />
+
+    <div class="project">
+      <div class="pattern-tab-bar">
+        {#each project["patterns"] as pattern, i}
+          <div
+            id={`${project["title"]}-${pattern.id}`}
+            class:current-tab={selectedPattern === pattern.index}
+            class="tab"
+            on:click={() => (selectedPattern = i)}
+          >
+            {`P${pattern.index}`}
+          </div>
+        {/each}
+      </div>
+      {#each project["patterns"] as pattern}
+        <div
+          class="pattern"
+          class:hidden={selectedPattern !== pattern.index}
+        >
+          {pattern.id}
+        </div>
+      {/each}
+    </div>
+  {/await}
 </div>
 
-<style></style>
+<style>
+  .project {
+    padding: 5px;
+    margin: auto;
+    width: 95vw;
+    height: 90vh;
+  }
+  .pattern-tab-bar {
+    display: flex;
+  }
+  .tab {
+    background-color: black;
+    color: white;
+    width: 50px;
+    height: 25px;
+    padding: 10px;
+    margin-right: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .current-tab {
+    color: black;
+    background-color: white;
+    width: 50px;
+    height: 25px;
+  }
+  .pattern {
+    /* width: 100%; */
+    margin-top: 15px;
+    height: 90%;
+    background-color: white;
+  }
+  .hidden {
+    display: none;
+  }
+</style>
