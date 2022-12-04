9e2d8c4caa5a05daedeb6720904bc995e9ea1815
Tom Amaral <thomasamaral2016@gmail.com>
2022-12-03 17:18:34 -0700

    Adjust properties, add some new ones

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,26 +1,29 @@
 export interface Step {
   active: boolean;
+  note: string;
   accent?: boolean;
   index: number;
 }
 
 export interface PatternState {
-  [note: string]: Step[];
+  [note: string]: Array<Step>;
 }
 
 export interface Pattern {
-  readonly id: number;
+  id: number;
   index: number;
   bpm?: number;
-  scale: string; // i.e. 'chromatic, 'c major', 'c# minor', etc
-  stepCount: number;
+  scale?: string; // i.e. 'chromatic, 'c major', 'c# minor', etc
+  stepCount?: number;
   /**(optional) How many steps per beat */
   subdivision?: number;
+  octaves?: number;
+  rootOctave?: number;
   state: PatternState;
 }
 
 export interface Project {
-  readonly id?: number;
+  id?: number;
   title: string;
   patterns: Pattern[];
 }
