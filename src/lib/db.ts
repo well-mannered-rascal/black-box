f62a8dbeb47d40109f44ed513a2622eb6bea45db
Tom Amaral <thomasamaral2016@gmail.com>
2022-12-03 17:17:38 -0700

    Add method to fetch a project and update project creation to insert empty inital pattern

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,9 +2,10 @@ import Dexie from "dexie";
 import type { Table } from "dexie";
 
 import type { Project } from "./types";
+import { newPattern } from "./util";
 
 export class BlackBoxDB extends Dexie {
-  project!: Table<Project>;
+  project!: Table<Project, number>;
   constructor() {
     super("blackboxDB");
     this.version(1).stores({
@@ -13,13 +14,16 @@ export class BlackBoxDB extends Dexie {
   }
 
   async createNewProject(title: string): Promise<string> {
-
     const id = await blackboxDB.project.add({
       title,
-      patterns: [],
+      patterns: [newPattern(0)],
     });
 
     return id.toString();
+  }
+
+  async getProject(projectId: number) {
+    return await this.project.get(projectId);
   }
 }
 
