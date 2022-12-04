826a70917086b84f7cb66499f9a207662228f1a8
Tom Amaral <thomasamaral2016@gmail.com>
2022-12-03 17:20:01 -0700

    Rough start on displaying a project's pattern and pattern state

--- src/views/Project.svelte
+++ src/views/Project.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-  import type { Project } from "../lib/types";
   import { blackboxDB } from "../lib/db";
   import AppHeader from "../components/AppHeader.svelte";
-  import { liveQuery } from "dexie";
 
   export let props;
   const projectId = parseInt(props.projectId);
 
-  let projectQuery = blackboxDB.project.get(projectId);
+  let projectPromise = blackboxDB.getProject(projectId);
+
   let selectedPattern = 0;
 </script>
 
 <div>
-  {#await projectQuery then project}
+  {#await projectPromise}
+    <AppHeader projectTitle={""} />
+  {:then project}
     <AppHeader projectTitle={project.title} />
-
     <div class="project">
       <div class="pattern-tab-bar">
-        {#each project["patterns"] as pattern, i}
+        {#each project.patterns as pattern, i}
           <div
-            id={`${project["title"]}-${pattern.id}`}
+            id={`${project.title}-${pattern.id}`}
             class:current-tab={selectedPattern === pattern.index}
             class="tab"
             on:click={() => (selectedPattern = i)}
           >
-            {`P${pattern.index}`}
+            {`${pattern.index + 1}`}
           </div>
         {/each}
       </div>
@@ -33,10 +33,25 @@
           class="pattern"
           class:hidden={selectedPattern !== pattern.index}
         >
-          {pattern.id}
+          {#each Object.entries(pattern.state) as [note, steps]}
+            <div class="note-row">
+              <span class="note">{note}</span>
+              <div class="steps">
+                {#each steps as step}
+                  <input
+                    class="step"
+                    type="checkbox"
+                    default={step.active}
+                  />
+                {/each}
+              </div>
+            </div>
+          {/each}
         </div>
       {/each}
     </div>
+  {:catch error}
+    {error}
   {/await}
 </div>
 
@@ -72,8 +87,34 @@
     margin-top: 15px;
     height: 90%;
     background-color: white;
+    overflow-y: auto;
+    overflow-x: auto;
+    position: relative;
   }
   .hidden {
     display: none;
+  }
+  .note-row {
+    display: flex;
+    height: 30px;
+  }
+  .note {
+    min-width: 50px;
+    height: 30px;
+    padding: 10px;
+    position: absolute;
+    background-color: black;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+  }
+  .steps {
+    margin-left: 60px;
+    display: flex;
+  }
+  .step {
+    margin-right: 5px;
+    height: 30px;
   }
 </style>
